26d11cd191a694b6fe9eecfb8e064d23cdc06362
Brian Nelson and David Zhu <david@>
2018-08-14 10:09:22 -0700

    add 2nd checknox question on the survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
 
-  $("#firstSurvey").submit(function(event) {
+  $("#Survey").submit(function(event) {
 
 
     $("input:checkbox[name=question-work]:checked").each(function () {
 
     alert($(this).val());
     });
+    $("input:checkbox[name=non-question-work]:checked").each(function () {
 
+    alert($(this).val());
+    });
 
 
     event.preventDefault();
